771eff2b977dcd338b624b7796e07b9a2c3de201
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2012-07-22 13:54:46 +0900

    add dataaccess-eventer project

--- service-eventer/src/main/java/com/pilgrim_lifestyle/service/eventer/EventerService.java
+++ service-eventer/src/main/java/com/pilgrim_lifestyle/service/eventer/EventerService.java
@@ -1,6 +1,14 @@
 package com.pilgrim_lifestyle.service.eventer;
 
+import com.pilgrim_lifestyle.model.eventer.Eventer;
+
+import org.springframework.stereotype.Service;
+
+@Service( "eventservice" )
 public class EventerService
 {
+    public void add( Eventer eventer )
+    {
 
+    }
 }
