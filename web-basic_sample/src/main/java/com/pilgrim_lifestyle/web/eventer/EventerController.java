838627395a843b15ad0945c600e23f09ff11d82b
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2012-07-25 15:16:40 +0900

    view webpage

--- web-basic_sample/src/main/java/com/pilgrim_lifestyle/web/eventer/EventerController.java
+++ web-basic_sample/src/main/java/com/pilgrim_lifestyle/web/eventer/EventerController.java
@@ -1,9 +1,18 @@
 package com.pilgrim_lifestyle.web.eventer;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
+@RequestMapping( "/" )
 public class EventerController
 {
-
+    @RequestMapping( value = "", method = RequestMethod.GET )
+    public String getEventer( WebRequest request, Model model )
+    {
+        return "eventer/register/register";
+    }
 }
