10e48a6f8a3e94d6ad2097ceeb3e0adf07f7ac84
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2012-08-17 02:12:22 +0900

    add test

--- service-eventer/src/test/java/com/pilgrim_lifestyle/service/eventer/AddEventerTest.java
+++ service-eventer/src/test/java/com/pilgrim_lifestyle/service/eventer/AddEventerTest.java
@@ -1,12 +1,4 @@
-package cucumber.eventer;
-
-import static org.junit.Assert.*;
-
-import mockit.Deencapsulation;
-import mockit.Expectations;
-import mockit.Mocked;
-
-import org.junit.Test;
+package com.pilgrim_lifestyle.service.eventer;
 
 import com.pilgrim_lifestyle.model.eventer.Eventer;
 import com.pilgrim_lifestyle.model.eventer.EventerRepository;
@@ -19,7 +11,14 @@ import com.pilgrim_lifestyle.model.eventer.security.Password;
 import com.pilgrim_lifestyle.model.eventer.security.Passwords;
 import com.pilgrim_lifestyle.service.eventer.EventerService;
 
-public class AA
+import mockit.Mocked;
+import mockit.NonStrictExpectations;
+import mockit.Verifications;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddEventerTest
 {
     private Contact contact;
     private Profile profile;
@@ -30,11 +29,11 @@ public class AA
     @Mocked
     private EventerRepository eventerRepository;
 
-    @Mocked( capture = 1 )
+    @Mocked(capture = 1)
     private Eventer eventer;
 
-    @Test
-    public void test()
+    @Before
+    public void setup()
     {
         PersonName personName = new PersonName( "田中", "達也" );
         TelephoneNumber telephoneNumber = new TelephoneNumber( "080-9999-1111" );
@@ -45,15 +44,31 @@ public class AA
         profile = new Profile( personName );
         passwords = new Passwords( password, confirm );
         contact = new Contact( mailAddress, telephoneNumber );
+    }
 
-        Deencapsulation.setField( eventerService, eventerRepository );
-        new Expectations()
+    @Test
+    public void 晴れの日_主催者を追加する()
+    {
+        new NonStrictExpectations()
         {{
+            setField( eventerService, eventerRepository );
             eventerRepository.nextId(); result = 1;
+
             eventerRepository.add( eventer );
+
         }};
 
         eventerService.add( contact, profile, passwords );
-    }
 
+        new Verifications()
+        {
+            {
+                eventerRepository.nextId();
+
+                eventerRepository.add( eventer );
+
+            }
+
+        };
+    }
 }
