63a3e4d552487fa543752d3756301816f9b2b773
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2012-08-19 03:40:46 +0900

    add loginform

--- web-basic_sample/src/main/java/com/pilgrim_lifestyle/web/dashboard/DashBoardController.java
+++ web-basic_sample/src/main/java/com/pilgrim_lifestyle/web/dashboard/DashBoardController.java
@@ -1,6 +1,7 @@
 package com.pilgrim_lifestyle.web.dashboard;
 
 import com.pilgrim_lifestyle.web.tool.OnetimeToken;
+import com.systemsekkei.base.security.web.login.LoginForm;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,12 +20,47 @@ public class DashBoardController
 
     private static final String DRAFT = "draft";
 
+    static final String LOGIN_FORM_SESSION_NAME = LoginForm.class.getName();
+
+    private String loginFormAttributeName = "loginForm";
+
     @RequestMapping( value = "", method = RequestMethod.GET )
     public String dashboard( WebRequest request, Model model )
     {
         onetimeToken.removeToken( request );
         request.removeAttribute( DRAFT, RequestAttributes.SCOPE_SESSION );
 
+        LoginForm loginForm = loadAndRemoveFromSession( request );
+
+        LoginForm preparedLoginForm = prepareShowLoginForm( loginForm, request );
+
+        request.setAttribute(
+                loginFormAttributeName, preparedLoginForm, WebRequest.SCOPE_REQUEST );
+
         return "dashboard/dashboard";
+    }
+
+    protected LoginForm prepareShowLoginForm(LoginForm loginForm, WebRequest request)
+    {
+        if (loginForm != null)
+        {
+            return loginForm;
+        }
+
+        return new LoginForm();
+    }
+
+    private static final LoginForm loadAndRemoveFromSession(WebRequest request)
+    {
+        LoginForm loginForm = (LoginForm) request.getAttribute(
+                LOGIN_FORM_SESSION_NAME, WebRequest.SCOPE_SESSION);
+        request.removeAttribute(
+                LOGIN_FORM_SESSION_NAME, WebRequest.SCOPE_SESSION);
+        return loginForm;
+    }
+
+    public void setLoginFormAttributeName(String loginFormAttributeName)
+    {
+        this.loginFormAttributeName = loginFormAttributeName;
     }
 }
