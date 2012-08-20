36e399757215d0cc187406ef27080472839a65fe
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2012-08-20 20:06:22 +0900

    aa

--- web-basic_sample/src/main/java/com/pilgrim_lifestyle/web/dashboard/DashBoardController.java
+++ web-basic_sample/src/main/java/com/pilgrim_lifestyle/web/dashboard/DashBoardController.java
@@ -2,7 +2,6 @@ package com.pilgrim_lifestyle.web.dashboard;
 
 import com.pilgrim_lifestyle.security.service.UserDetailsService;
 import com.pilgrim_lifestyle.web.tool.OnetimeToken;
-import com.pilgrim_lifestyle.web.dashboard.LoginForm;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +19,6 @@ public class DashBoardController
 
     private static final String DRAFT = "draft";
 
-    static final String LOGIN_FORM_SESSION_NAME = LoginForm.class.getName();
-
-    private String loginFormAttributeName = "loginForm";
-
-    private String viewName = "dashboard/dashboard";
-
     @RequestMapping( value = "dashboard" )
     public String dashboard( WebRequest request, Model model )
     {
@@ -33,43 +26,8 @@ public class DashBoardController
         onetimeToken.removeToken( request );
         request.removeAttribute( DRAFT, RequestAttributes.SCOPE_SESSION );
 
-        LoginForm loginForm = loadAndRemoveFromSession( request );
-
-        LoginForm preparedLoginForm = prepareShowLoginForm( loginForm, request );
-
-        request.setAttribute( loginFormAttributeName, preparedLoginForm, WebRequest.SCOPE_REQUEST );
-
         logger.info(  "dashboard end" );
-        return viewName;
-    }
-
-    protected LoginForm prepareShowLoginForm(LoginForm loginForm, WebRequest request)
-    {
-        if (loginForm != null)
-        {
-            return loginForm;
-        }
-
-        return new LoginForm();
-    }
-
-    private static final LoginForm loadAndRemoveFromSession(WebRequest request)
-    {
-        LoginForm loginForm = (LoginForm) request.getAttribute(
-                LOGIN_FORM_SESSION_NAME, WebRequest.SCOPE_SESSION);
-        request.removeAttribute(
-                LOGIN_FORM_SESSION_NAME, WebRequest.SCOPE_SESSION);
-        return loginForm;
-    }
-
-    public void setLoginFormAttributeName(String loginFormAttributeName)
-    {
-        this.loginFormAttributeName = loginFormAttributeName;
-    }
-
-    public void setViewName(String viewName)
-    {
-        this.viewName = viewName;
+        return "dashboard/dashboard";
     }
 
     private static Logger logger= Logger.getLogger( UserDetailsService.class );
