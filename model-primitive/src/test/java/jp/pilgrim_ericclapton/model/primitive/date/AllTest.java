7ff0c609d34e93fe799828572a9f7d9e412807ef
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2012-09-11 23:48:38 +0900

    add test

--- model-primitive/src/test/java/jp/pilgrim_ericclapton/model/primitive/date/AllTest.java
+++ model-primitive/src/test/java/jp/pilgrim_ericclapton/model/primitive/date/AllTest.java
@@ -1,5 +1,8 @@
 package jp.pilgrim_ericclapton.model.primitive.date;
 
+import jp.pilgrim_ericclapton.model.primitive.date.format.DateStampFormatTest;
+import jp.pilgrim_ericclapton.model.primitive.date.format.TimeStampFormatTest;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -9,6 +12,8 @@ import org.junit.runners.Suite.SuiteClasses;
         DateStampTest.class
         , DateStampParsingTest.class
         , TimeStampTest.class
+        , DateStampFormatTest.class
+        , TimeStampFormatTest.class
 } )
 public class AllTest
 {
