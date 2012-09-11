b0c75be9e8bf63d96ccc386072e6f5688859c5ea
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2012-09-11 16:37:34 +0900

    add test

--- model-primitive/src/test/java/jp/pilgrim_ericclapton/model/primitive/date/AllTest.java
+++ model-primitive/src/test/java/jp/pilgrim_ericclapton/model/primitive/date/AllTest.java
@@ -7,6 +7,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith( value = Suite.class )
 @SuiteClasses( value = {
         DateStampTest.class
+        , DateStampParsingTest.class
+        , TimeStampTest.class
 } )
 public class AllTest
 {
