f48d2f4463adb96c200bb5b4e8c332b9a28023d9
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2012-10-09 03:14:24 +0900

    event test

--- model-event/src/test/java/com/pilgrim_lifestyle/model/event/EventData.java
+++ model-event/src/test/java/com/pilgrim_lifestyle/model/event/EventData.java
@@ -1,5 +1,7 @@
 package com.pilgrim_lifestyle.model.event;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.EnumMap;
 
 public class EventData
@@ -18,11 +20,13 @@ public class EventData
         , APPLICATION_DATE_RANGE_END_TIME;
     }
 
-    static private EnumMap<Data, String> data = new EnumMap<Data, String>( Data.class );
-    static
+    private EnumMap<Data, String> data = new EnumMap<Data, String>( Data.class );
+
+    public EventData()
     {
         data.put( Data.ID, "1" );
         data.put( Data.NAME, "勉強会" );
+        data.put( Data.HeadCount, "30" );
         data.put( Data.EXPLANATION, "Javaの勉強会です" );
         data.put( Data.DATEOF_DATE, "2012/10/01" );
         data.put( Data.DATEOF_TIME, "17:00" );
@@ -31,8 +35,22 @@ public class EventData
         data.put( Data.APPLICATION_DATE_RANGE_END_DATE, "2012/09/01" );
         data.put( Data.APPLICATION_DATE_RANGE_END_TIME, "17:00" );
     }
-    public static EnumMap<Data, String> getData()
+
+    public EnumMap<Data, String> getData()
     {
         return data;
+    }
+
+    public static String createStringDate( int count )
+    {
+        String result = "";
+        for( int i = 0; i < count; i++ )
+        {
+            result = result + "a";
+        }
+
+        assertTrue( result.length() == count );
+
+        return result;
     }
 }
--- model-event/src/test/java/com/pilgrim_lifestyle/model/event/EventTest.java
+++ model-event/src/test/java/com/pilgrim_lifestyle/model/event/EventTest.java
@@ -2,38 +2,47 @@ package com.pilgrim_lifestyle.model.event;
 
 import java.util.Map;
 
+import javax.validation.constraints.Size;
+
 import com.systemsekkei.base.test.model.BaseModelTest;
 
-import org.junit.Before;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
 
-
-@RunWith( Enclosed.class )
-public class EventTest
+public class EventTest extends BaseModelTest<Event>
 {
+    private final Map<EventData.Data, String> eventData = new EventData().getData();
 
-    public static class validationが起動するか extends BaseModelTest<Event>
+    private Event event = null;
+
+    @Test
+    public void eventDetailのvalidationが起動するか()
     {
-        private Event event = null;
+        eventData.put( EventData.Data.HeadCount, "" );
 
-        @Before
-        public void setup()
-        {
-            Map<EventData.Data, String> eventData = EventData.getData();
-            eventData.put( EventData.Data.ID, "0" );
-            eventData.put( EventData.Data.NAME, "" );
-            eventData.put( EventData.Data.HeadCount, "" );
+        event = CreateEvent.createEvent( eventData );
 
-            event = CreateEvent.createEvent( eventData );
-        }
+        validateAndAssertCount( 1, event );
+    }
 
-        @Test
-        public void validationが起動する()
-        {
-            validateAndAssertCount( 2, event );
-        }
+    @Test
+    public void nameはnotEmpty()
+    {
+        eventData.put( EventData.Data.NAME, "" );
+
+        event = CreateEvent.createEvent( eventData );
+
+        validateAndAssert( "name", NotEmpty.class, event );
+    }
+
+    @Test
+    public void nameは51文字だとエラー()
+    {
+        eventData.put( EventData.Data.NAME, EventData.createStringDate( 51 ) );
+
+        event = CreateEvent.createEvent( eventData );
+
+        validateAndAssert( "name", Size.class, event );
     }
 
 }
