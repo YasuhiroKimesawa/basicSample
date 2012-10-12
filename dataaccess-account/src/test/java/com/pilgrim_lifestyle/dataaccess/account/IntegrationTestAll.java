3536483e4e3e43053ea6f4a6cf3f8538cb144d96
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2012-10-12 17:17:38 +0900

    add dataaccess account test alltest.java

---
+++ dataaccess-account/src/test/java/com/pilgrim_lifestyle/dataaccess/account/IntegrationTestAll.java
@@ -0,0 +1,14 @@
+package com.pilgrim_lifestyle.dataaccess.account;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith( value = Suite.class )
+@SuiteClasses( value = {
+        AccountDataAccessTest.class
+})
+public class IntegrationTestAll
+{
+
+}
