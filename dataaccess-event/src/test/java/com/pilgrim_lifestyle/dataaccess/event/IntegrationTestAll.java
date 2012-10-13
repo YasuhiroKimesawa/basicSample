67530c1f1fec3a959b3d8366db075ab60182d910
YasuhiroKimesawa <pilgrim.ericclapton@gmail.com>
2012-10-13 23:46:06 +0900

    modify test build

---
+++ dataaccess-event/src/test/java/com/pilgrim_lifestyle/dataaccess/event/IntegrationTestAll.java
@@ -0,0 +1,14 @@
+package com.pilgrim_lifestyle.dataaccess.event;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith( value = Suite.class )
+@SuiteClasses( value = {
+        EventDataAccessTest.class
+})
+public class IntegrationTestAll
+{
+
+}
